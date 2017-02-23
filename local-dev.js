ade2459be836cc42048576ec3ac933cb5a3ddf43
James R Sconfitto <james@exele.com>
2017-02-23 14:05:53 -0500

    Bind Update button after importing HTML

--- local-dev.js
+++ local-dev.js
@@ -1,10 +1,17 @@
-﻿
-var apiServer = "https://dan-af-dev/piwebapi";
+﻿var apiServer = "https://dan-af-dev/piwebapi";
 var elementPath = '\\\\DAN-AF-DEV\\Mineral Processing\\Toll Ore Delivery\\T-101';
 var tStart = "*-1mo";
 var tEnd = "*";
 
-// creates an internal element object that the module uses
-$('#btnUpdate').click(function () {
-    eventsModule.Update(apiServer, elementPath, tStart, tEnd);
-});
+function handleLoad(e) {
+    var importedContent = document.querySelector('link[rel="import"]').import;
+
+    // creates an internal element object that the module uses
+    $('#btnUpdate').click(function () {
+        eventsModule.Update(apiServer, elementPath, tStart, tEnd);
+    }.bind(importedContent));
+}
+
+function handleError(e) {
+    console.log('Error loading import: ' + e.target.href);
+}
