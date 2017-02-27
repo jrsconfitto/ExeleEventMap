7f3b26b5593c322d2b09f982cddceb5f189fe291
James R Sconfitto <james@exele.com>
2017-02-27 16:49:02 -0500

    Format document

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var eventsModule = function () {
     // Modeled after the example given in Mike Bostock's fantastic "Towards Reusable Charts": https://bost.ocks.org/mike/chart/
     //
     // This pattern allows us to create (lots of) treemaps easily and update their data (and other attributes) by calling this function with new data (or new chart attributes).
-    
+
     function treemap() {
         var width = 960,
             height = 570;
@@ -164,27 +164,25 @@ var eventsModule = function () {
         this.framesLink = framesLink;
     }
     // used to return all of the EF templates used as array
-    function GetTemplates()
-    {
+    function GetTemplates() {
         templates = ["None"];
-        for(var t in efDataHolder){
-        templates.push(t);
+        for (var t in efDataHolder) {
+            templates.push(t);
         }
-        return templates;     
+        return templates;
     }
     // use to return the attributes as array given a template
-    function GetEFAttributesFromTemplate(templateName){
-        var attResults =["None"];
+    function GetEFAttributesFromTemplate(templateName) {
+        var attResults = ["None"];
         var attributes = [];
-        if (efDataHolder[templateName])
-        {
+        if (efDataHolder[templateName]) {
             attributes = efDataHolder[templateName].attributeNames;
         }
-      
+
         return ["None"].concat(attributes);
     }
-   
-  
+
+
 
     // main function that builds up the EF data
     // gets the element, gets the EF on the element
@@ -195,10 +193,10 @@ var eventsModule = function () {
 
         // get the results, create a mock element, and call function to get the EF
         function PathResults(results) {
-            myel = new myElement(results.Name, results.Path, results.WebId, results.Links.EventFrames);           
+            myel = new myElement(results.Name, results.Path, results.WebId, results.Links.EventFrames);
             GETEFByElementID(myel.framesLink, startTime, endTime, ExtractEF);
         }
-         // get the resulting EF within the time range, and calls ExtractEF when completed
+        // get the resulting EF within the time range, and calls ExtractEF when completed
         function GETEFByElementID(elementIDbase, startTime, endtime, successCallBack) {
             url = elementIDbase + "?StartTime=" + startTime + "&" + "Endtime=" + endtime + "&searchmode=StartInclusive";
             this.symbolElement = symbolElement;
@@ -224,11 +222,11 @@ var eventsModule = function () {
             let EF = new myEventFrame(apiFrameResult.Name, apiFrameResult.TemplateName, apiFrameResult.StartTime, apiFrameResult.EndTime,
                 apiFrameResult.Links.Template, apiFrameResult.WebId);
             // if the EF template is not a property of the object, add it
-            if (efDataHolder[EF.templateName] === undefined) {               
-                
+            if (efDataHolder[EF.templateName] === undefined) {
+
                 efDataHolder[EF.templateName] = {
                     "Links": apiFrameResult.Links.Template,
-                    "frames": [],                   
+                    "frames": [],
                 }
             }
             // for all EF, add an arry of the EF with properties of id and the actual EF object
@@ -239,9 +237,9 @@ var eventsModule = function () {
         }
         // get the attribute tepmlates to cache them here to show in the grid, we should move to own cache
         GetAllTemplateAttributes();
-      
+
         // Get attribute value for provide attribute and template.
-        if (_attribute != undefined &&_attribute != "None" && _template !="None") {
+        if (_attribute != undefined && _attribute != "None" && _template != "None") {
             GetAttributesValues(_attribute, _template);
         }
         //reference the treeview and build it here
@@ -272,7 +270,7 @@ var eventsModule = function () {
                     efDataHolder[templates].attributeNames = attributesNames;
                 }
             }
-        }           
+        }
     }
 
 
@@ -317,7 +315,7 @@ var eventsModule = function () {
             // probably should return the attribute array here
             console.log(attributes);
         }, error=>console.log(error));
-      
+
     }
 
     // get the attribute values for each EF given an attributeName and template
@@ -348,7 +346,7 @@ var eventsModule = function () {
     // takes batch call results, and adds values to the correct EF
     function ProcessAttributeResults(results, templateName, attributeName) {
         for (let result in results) {
-            if (results[result].Status == 200 && results[result].Content.Items.length > 0) {               
+            if (results[result].Status == 200 && results[result].Content.Items.length > 0) {
                 const attributeMap = new Map();
                 // add attribute values to the map.
                 attributeMap.set(attributeName, results[result].Content.Items[0].Value.Value);
@@ -412,8 +410,7 @@ var eventsModule = function () {
     }
 
     // sets the tree symbol
-    function SetSymbol(treeSymbolElement)
-    {
+    function SetSymbol(treeSymbolElement) {
         symbolElement = treeSymbolElement;
     }
     // sets the WebAPI server
@@ -430,27 +427,26 @@ var eventsModule = function () {
 
     // ----------public methods---------------------------------------------
     //----------------------------------------------------------------------
-    return {       
+    return {
         // gets all of the EF attributes givena  template, need to extended to use attribute name
         GetEFAttributesValuesFromTemplate: (apiServer, templateName) =>GetTemplateAttributes(apiServer, templateName),
         // Creates an element object provided a path
-        Update: (APIServer, elementPath, symbolElement, startTime, endTime, template, attribute) => {          
-             // store the symbol and the apiserver as private variables in the module, we should initiallize first.
+        Update: (APIServer, elementPath, symbolElement, startTime, endTime, template, attribute) => {
+            // store the symbol and the apiserver as private variables in the module, we should initiallize first.
             SetSymbol(symbolElement);
             SetWebAPIURL(APIServer);
             SetTemplate(template);
             SetAttribute(attribute);
-            
+
             // obtain the EF data
-            GetEFData(elementPath, startTime, endTime);               
+            GetEFData(elementPath, startTime, endTime);
         },
         // get EF templates
-        GetEFTemplates: ()=>
-        {
-          return  GetTemplates();
+        GetEFTemplates: () => {
+            return GetTemplates();
         },
         // get the Attributes provide a tepmlate
-        GetEFAttributesFromTemplate: (templateName)=>{
+        GetEFAttributesFromTemplate: (templateName) => {
             return GetEFAttributesFromTemplate(templateName);
 
         },
